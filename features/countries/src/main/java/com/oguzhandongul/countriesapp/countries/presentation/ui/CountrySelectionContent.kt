31d13b3f535c1ac40c8a5d9a9d261f2a777eb6f1
oguzhandongul <oguzhandongul@gmail.com>
2024-04-11 17:50:29 +0200

    add headline for country list screen

--- features/countries/src/main/java/com/oguzhandongul/countriesapp/countries/presentation/ui/CountrySelectionContent.kt
+++ features/countries/src/main/java/com/oguzhandongul/countriesapp/countries/presentation/ui/CountrySelectionContent.kt
@@ -1,12 +1,17 @@
 package com.oguzhandongul.countriesapp.countries.presentation.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.oguzhandongul.countriesapp.core.ui.theme.CountriesTypography
+import com.oguzhandongul.countriesapp.core.ui.theme.Dimensions
 
 @Composable
 fun CountriesListContent(navController: NavController, countryList: List<String>) {
@@ -15,6 +20,13 @@ fun CountriesListContent(navController: NavController, countryList: List<String>
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        item {
+            Text(
+                text = "Choose a Country:",
+                modifier = Modifier.fillMaxWidth().padding(Dimensions.xlarge),
+                style = CountriesTypography.headlineMedium
+            )
+        }
         items(countryList) { countryName ->
             CountryItem(name = countryName, navController = navController)
         }
